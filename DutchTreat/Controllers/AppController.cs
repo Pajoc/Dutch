438d256430ebeab577aba8b3c36253f0b9a28220
Pajoc <pajoc527@gmail.com>
2018-11-21 21:05:34 +0000

    Add project files.

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,54 @@
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+
+        public AppController(IMailService mailservice)
+        {
+            _mailService = mailservice;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact us";
+            //throw new InvalidOperationException("Upps"); 
+
+            return View();
+        }
+        [HttpPost("contact")]
+        public ActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("p@gmail.com", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail sent";
+                ModelState.Clear();
+            }
+           
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //1- AddTransient não tem dados apenas métodos para efetuar operações,AddScoped inserir conn e AddSingleton criados uma vez
+            services.AddTransient<IMailService, NullMailService>();
+            // Support for real mail service...
+
+            //Dependency injection é obrigatório
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //app.UseDefaultFiles();
+
+
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
+            //Ao procurarpor um match segue este padrão compara o primeiro elemento do url e vê se é um controlo conhecido, depois a view
+            //o new é val por defeito quando nada é introduzido
+            app.UseMvc(cfg => {
+                cfg.MapRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
+        }
+    }
+}
