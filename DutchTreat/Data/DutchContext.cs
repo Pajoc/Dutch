9e5588cdc14245ab31e1fe8b1e39f45044bef5be
PajocCasa <pajoc527@gmail.com>
2018-11-25 12:31:23 +0000

    Com connString

---
+++ DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,18 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DutchContext(DbContextOptions<DutchContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Orders { get; set; }
+
+    }
+}
--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -19,7 +19,18 @@ namespace DutchTreat
 
     public static IWebHost BuildWebHost(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(SetupConfiguration)
             .UseStartup<Startup>()
             .Build();
-  }
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            //Remove as opções de configuração por defeito
+            builder.Sources.Clear();
+            //assim fica mais versátil permite ter vários formatos de ficheiros de configuração
+            builder.AddJsonFile("config.json", false, true)
+                .AddXmlFile("config.xml", true)
+                .AddEnvironmentVariables();//Junta todos esses ficheiros de config. num só (em caso de conflitos prevalece o último)!
+        }
+    }
 }
